9cf94d8a932f627a30811207013723ff1469fdc1
Jignesh Patel <jpate605@my.centennialcollege.ca>
2023-11-07 21:25:08 -0500

    Add project files.

---
+++ MVC Agile Process/Data/MVC_Agile_ProcessContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Agile_Process.Data
+{
+    public class MVC_Agile_ProcessContext : DbContext
+    {
+        public MVC_Agile_ProcessContext(DbContextOptions<MVC_Agile_ProcessContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC_Agile_Process.Models.Project> Project { get; set; } = default!;
+    }
+}
---
+++ MVC Agile Process/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC_Agile_Process.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
